5b50246d580a5a17478ae236975242cf4a189e72
LordAnthrax94 <andreagi1994@gmail.com>
2025-06-02 14:11:49 +0200

    Fix: impostato file env, creato context e impostato fetch per chiamata get

---
+++ src/context/GlobalContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState, useEffect, use } from "react";
+
+const GlobalContext = createContext();
+const GlobalProvider = ({children}) =>{
+
+const api_url = import.meta.env.VITE_API_URL;
+
+const [tasks, setTasks] = useState([]);
+
+const fetchTasks = () =>{
+  fetch(`${api_url}/tasks`)
+    .then((response) => response.json())
+    .then((data) => setTasks(data))
+    .catch((error) => console.error("Error fetching tasks:", error));
+};
+
+useEffect(() => {
+  fetchTasks();}, []);
+
+  return (
+        <GlobalContext.Provider value={value}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+const useGlobalContext = () => useContext(GlobalContext)
+
+export { useGlobalContext, GlobalProvider }
+
