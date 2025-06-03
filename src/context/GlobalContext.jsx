d71da6a4d33483b7e89ee09d0ffb6b9dafd3322b
LordAnthrax94 <andreagi1994@gmail.com>
2025-06-03 10:19:42 +0200

    Fix: corretto installazione pacchetto axios e modalità di fetch

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { createContext, useContext, useState, useEffect, use } from "react";
 
 const GlobalContext = createContext();
@@ -9,9 +8,9 @@ const api_url = import.meta.env.VITE_API_URL;
 const [tasks, setTasks] = useState([]);
 
 const fetchTasks = () =>{
-  axios
-  .get(`${api_url}/tasks`)
-  .then((res) =>{setTasks(res.data)})
+  fetch(`${api_url}/tasks`)
+  .then((response) => response.json())
+  .then((data) => setTasks(data))
   .catch((error) => console.error("Error fetching tasks:", error));    
 };
 
